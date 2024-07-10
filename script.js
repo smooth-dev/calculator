7e862e37d0ac5e7a3f6572f83496701b523ab88c
Azhar <yassine.azhar@accenture.com>
2024-07-10 16:52:53 +0100

    added equald functionnality

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
 let currentScreen = document.querySelector(".operation"); //current operation (should just hold current number or resukt after operation calculation)
 
-let resultContainer = document.querySelector(".result"); // shows ongoing calculation at all times
+let resultScreen = document.querySelector(".result"); // shows ongoing calculation at all times
 
 let numbersContainer = document.querySelector(".numbers"); // click event on  a number
 
 let operationsContainer = document.querySelector(".operations"); // click event on + - / %
+
+let deleteOneContainer = document.querySelector(".deleteOne"); // teh button that deletes on char
+
+
 
 let currentNumber,
   calculatedNumber = 0;
 let currentOperation, lastOperation;
 let lastWasOperation = false;
 numbersContainer.addEventListener("click", (v) => {
-  // if (lastOperation == undefined) {
   currentScreen.innerText += v.target.innerText;
   currentNumber = +currentScreen.innerText;
-  // } else {
-  //   {
-  //     currentScreen.innerText = v.target.innerText;
-  //     currentNumber = +currentScreen.innerText;
-  //   }
-  //}
+ 
   if (lastWasOperation) {
     currentScreen.innerText = v.target.innerText;
     currentNumber = +currentScreen.innerText;
   }
 
-  resultContainer.innerText += v.target.innerText;
+  resultScreen.innerText += v.target.innerText;
   lastWasOperation = false;
 });
 
 operationsContainer.addEventListener("click", (v) => {
   lastWasOperation = true;
   currentOperation = v.target.innerText;
-  resultContainer.innerText += " " + currentOperation;
-  if (lastOperation != undefined) {
+  resultScreen.innerText += " " + currentOperation;
+  if (lastOperation != undefined ) {
     switch (lastOperation) {
       case "+":
         calculatedNumber = calculatedNumber + currentNumber;
@@ -54,7 +52,7 @@ operationsContainer.addEventListener("click", (v) => {
     }
     currentNumber = calculatedNumber;
     currentScreen.innerText = calculatedNumber;
-    resultContainer.innerText = currentNumber + currentOperation;
+    resultScreen.innerText = currentNumber + (currentOperation!="="?currentOperation:"");
     lastOperation = currentOperation;
 
     console.log(calculatedNumber);
@@ -72,3 +70,17 @@ operationsContainer.addEventListener("click", (v) => {
       calculatedNumber
   );
 });
+
+
+
+deleteOneContainer.addEventListener("click",(event)=>{
+  if(lastWasOperation){ lastOperation=undefined;}
+  else {
+    currentScreen.innerText =currentScreen.innerText.slice(0, -1)
+    currentNumber=Math.floor(currentNumber/10);
+  }
+
+  resultScreen.innerText=resultScreen.innerText.slice(0, -1);
+
+  
+})
