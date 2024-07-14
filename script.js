a14e17be5040ee3269878d9f5084d676961549a7
Yassine Azh <azhar.yassine2015@gmail.com>
2024-07-14 22:46:24 +0100

    final commit cleaned logs, dead code, added comments

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let currentScreen = document.querySelector(".operation"); //current operation (should just hold current number or resukt after operation calculation)
+let currentScreen = document.querySelector(".operation"); //current operation (holds current number or result after operation calculation)
 
 let resultScreen = document.querySelector(".result"); // shows ongoing calculation at all times
 
@@ -6,35 +6,37 @@ let numbersContainer = document.querySelector(".numbers"); // click event on  a 
 
 let operationsContainer = document.querySelector(".operations"); // click event on + - / %
 
-let deleteOneContainer = document.querySelector(".deleteOne"); // teh button that deletes on char
+let deleteOneContainer = document.querySelector(".deleteOne"); // deletes one char
 
-let deleteAllContainer = document.querySelector(".deleteAll"); // teh button that deletes on char
+let deleteAllContainer = document.querySelector(".deleteAll"); //  deletes all
 
-let cursorElement = document.getElementById("cursor");
+let cursorElement = document.getElementById("cursor"); // blinking cursor
+
+// blinking cursor animation
 setInterval(() => {
-  if (cursorElement.style.visibility === 'hidden') {
-    cursorElement.style.visibility = 'visible';
+  if (cursorElement.style.visibility === "hidden") {
+    cursorElement.style.visibility = "visible";
   } else {
-      cursorElement.style.visibility = 'hidden';
+    cursorElement.style.visibility = "hidden";
   }
-}, 500); 
-
-
+}, 500);
 
 let currentNumber,
   calculatedNumber = 0;
 let currentOperation, lastOperation;
-let lastWasOperation = false;
+let lastWasOperation = false; // last clicked button was / x + -
+
 numbersContainer.addEventListener("click", (v) => {
+  if (v.target.innerText.length > 1) return; // in case click on the container instead of a button (which would trigger all numbers at once)
   currentScreen.innerText += v.target.innerText;
   currentNumber = +currentScreen.innerText;
- 
+
   if (lastWasOperation) {
     currentScreen.innerText = v.target.innerText;
     currentNumber = +currentScreen.innerText;
   }
 
-  resultScreen.childNodes[0].nodeValue  += v.target.innerText;
+  resultScreen.childNodes[0].nodeValue += v.target.innerText;
   lastWasOperation = false;
 });
 
@@ -42,10 +44,10 @@ operationsContainer.addEventListener("click", (v) => {
   lastWasOperation = true;
   currentOperation = v.target.innerText;
   resultScreen.childNodes[0].nodeValue += " " + currentOperation;
-  if (lastOperation != undefined ) {
+  if (lastOperation != undefined) {
     switch (lastOperation) {
       case "+":
-        calculatedNumber = calculatedNumber + currentNumber;
+        calculatedNumber += currentNumber;
 
         break;
       case "-":
@@ -56,63 +58,39 @@ operationsContainer.addEventListener("click", (v) => {
         calculatedNumber /= currentNumber;
 
         break;
-      case "*":
+      case "x":
         calculatedNumber *= currentNumber;
 
         break;
     }
     currentNumber = calculatedNumber;
     currentScreen.innerText = calculatedNumber;
-    console.log("resultScreen.innerText"+resultScreen.childNodes+"currentNumber"+currentNumber+"/currentOperation"+currentOperation)
-    resultScreen.childNodes[0].nodeValue  = currentNumber + (currentOperation!="="?currentOperation:"");
+    resultScreen.childNodes[0].nodeValue =
+      currentNumber + (currentOperation != "=" ? currentOperation : "");
     lastOperation = currentOperation;
-
-    console.log(calculatedNumber);  
   } else {
     calculatedNumber = currentNumber;
     lastOperation = v.target.innerText;
   }
-
-  console.log(
-    "lastOperation" +
-      lastOperation +
-      "currentOperation" +
-      currentOperation +
-      "calculatedNumber" +
-      calculatedNumber
-  );
 });
 
-
-
-deleteOneContainer.addEventListener("click",(event)=>{
-  if(lastWasOperation){ lastOperation=undefined;
-    lastWasOperation=false;
-  }
-  else {
-    currentScreen.innerText =currentScreen.innerText.slice(0, -1)
-    currentNumber=Math.floor(currentNumber/10);
+deleteOneContainer.addEventListener("click", (event) => {
+  if (lastWasOperation) {
+    lastOperation = undefined;
+    lastWasOperation = false;
+  } else {
+    currentScreen.innerText = currentScreen.innerText.slice(0, -1);
+    currentNumber = Math.floor(currentNumber / 10);
   }
 
-  resultScreen.childNodes[0].nodeValue=resultScreen.childNodes[0].nodeValue.slice(0, -1);
+  resultScreen.childNodes[0].nodeValue =
+    resultScreen.childNodes[0].nodeValue.slice(0, -1);
+});
 
-  
-})
-
-
-deleteAllContainer.addEventListener("click",(event)=>{
-  resultScreen.childNodes[0].nodeValue="";
-  currentScreen.innerText="";
-  currentNumber="";
-  currentOperation="";
-  lastOperation=undefined;
-  
-
-  
-})
-
-
-
-
-
-
+deleteAllContainer.addEventListener("click", (event) => {
+  resultScreen.childNodes[0].nodeValue = "";
+  currentScreen.innerText = "";
+  currentNumber = "";
+  currentOperation = "";
+  lastOperation = undefined;
+});
